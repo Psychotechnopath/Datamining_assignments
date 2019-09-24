f750f825034d0935b8a28c80a028989b30abf53a
Psychotechnopath <ymevdlinden@gmail.com>
2019-09-24 11:50:15 +0200

    + comment

--- Assignment 1/yme_knn.py
+++ Assignment 1/yme_knn.py
@@ -8,10 +8,16 @@ X, y, cats, attrs = SVHN.get_data(dataset_format='array',
 
 X_90_percent, X_10_percent, y_90_percent, y_10_percent = train_test_split(X, y, test_size=0.1, stratify=y)
 
+#Evaluate k-Nearest Neighbors, using default hyperparameter settings. Use cross-validation with 3 folds,
+#output the training accuracy and test accuracy including the standard deviations
+
+#Initialize default knn classifier
 knn = KNeighborsClassifier()
+#Use cross_validate with regular CV (As sample is already stratified, and we were not asked to use StratifiedKfold)
 scores = cross_validate(knn, X_10_percent, y_10_percent, cv=3, scoring=['accuracy'], return_train_score=True)
 
-print("Training accuracy of models {}".format(scores['train_accuracy']))
-print("Standard deviation of training accuracies are {}".format(scores['train_accuracy'].std()))
-print("Testing accuracy of models {}".format(scores['test_accuracy']))
-print("Standard deviation of Training accuracies {}".format(scores['test_accuracy'].std()))
+#Report training accuracy + std, testing accuracy + std
+print("Training accuracy of models {}".format(scores['train_accuracy']))            #Training accuracy for 3 folds:
+print("Standard deviation of training accuracies are {}".format(scores['train_accuracy'].std())) #std over 3 folds:
+print("Testing accuracy of models {}".format(scores['test_accuracy']))               #Testing accuracy for 3 folds:
+print("Standard deviation of Training accuracies {}".format(scores['test_accuracy'].std()))      #std over 3 folds:

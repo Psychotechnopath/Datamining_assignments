8242a2a0a09a00e5fce716689aaf27a34becee58
Psychotechnopath <ymevdlinden@gmail.com>
2019-09-29 12:43:01 +0200

    Finished question 3 codes. Will upload results later.

--- Assignment 1/yme_svm.py
+++ Assignment 1/yme_svm.py
@@ -3,6 +3,8 @@ import openml as oml
 from sklearn.svm import LinearSVC
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import LogisticRegression
+from sklearn.decomposition import PCA
+from sklearn.pipeline import Pipeline
 import numpy as np
 
 SVHN = oml.datasets.get_dataset(41081)                                   #Load data
@@ -20,15 +22,15 @@ knn = KNeighborsClassifier()
 #Initialize default linear support vector machine
 svc = LinearSVC()
 
-
+pipeline= Pipeline([("PCA", PCA(n_components='mle', svd_solver='full')), ("svm", LinearSVC())])
+print(pipeline.named_steps["PCA"])
 
 X_gray_std_90_percent, X_gray_std_10_percent, y_90_percent, y_10_percent = train_test_split(X_gray_std, y, test_size=0.1, stratify=y, random_state=47)
 
 #Use cross_validate with regular CV (As sample is already stratified, and we were not asked to use StratifiedKfold)
-scores = cross_validate(logisticReg, X_gray_std_10_percent, y_10_percent, cv=3, scoring=['accuracy'], return_train_score=True)
+scores = cross_validate(pipeline, X_gray_std_10_percent, y_10_percent, cv=3, scoring=['accuracy'], return_train_score=True)
 
-
-
+print(pipeline.named_steps["PCA"].n_components_)
 #Report training accuracy + std, testing accuracy + std
 print("Training accuracy of models {}".format(scores['train_accuracy']))            #Training accuracy for 3 folds:
 print("Standard deviation of training accuracies are {}".format(scores['train_accuracy'].std())) #std over 3 folds:

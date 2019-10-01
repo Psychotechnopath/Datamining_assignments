d87ffed36be9010e5ba44b8d995332686d1b81c0
Psychotechnopath <ymevdlinden@gmail.com>
2019-10-01 14:27:25 +0200

    Added pickled results

--- Assignment 1/cache/result_checker.py
+++
@@ -1,7 +0,0 @@
-import pickle
-import numpy as np
-
-results_file = open('C:/Users/Gebruiker/MEGA/Master DSE/Data Mining/Assignments/Assignment 1/cache/logreg_pca_score60factors.pkl','rb')
-cv_results_ = pickle.load(results_file)
-print(np.mean(cv_results_['test_acc']))
-results_file.close()

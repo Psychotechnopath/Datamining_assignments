de5ff32910911a7dac9f71a3dbd8416c0c8a0d43
Psychotechnopath <ymevdlinden@gmail.com>
2019-10-22 11:49:58 +0200

    This is the first commit of the second assignment

--- Assignment 1/yme_q4_gridsearch.py
+++ Assignment 1/yme_q4_gridsearch.py
@@ -5,40 +5,41 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import LogisticRegression
 import numpy as np
 import pickle
+import mglearn
 
 
-SVHN = oml.datasets.get_dataset(41081)                                   #Load data
-X, y, cats, attrs = SVHN.get_data(dataset_format='array',
-    target=SVHN.default_target_attribute)
-
-X_gray_std = np.load("std_grayscale_data.npy")
-
-#Initialize default logistic regression
-logisticReg = LogisticRegression()
-
-#Initialize default knn classifier
-knn = KNeighborsClassifier()
-
-#Initialize default linear support vector machine
-svc = LinearSVC()
-
-X_gray_std_90_percent, X_gray_std_10_percent, y_90_percent, y_10_percent = train_test_split(X_gray_std, y, test_size=0.1, stratify=y, random_state=47)
-
-
-param_grid_course = {'C': [1e-5 ,1e-4, 1e-3, 1e-2 ,1e-1 ,1, 1e2, 1e3, 1e4, 1e5]}
-param_grid2 = {'n_neighbors': [5,15,25,35,45]}
-# using the default scoring of accuracy:
-
-#In GridSearchCV we use the default cv, as it implements Stratified K-folds cross-validation with three folds,
-# From documentation: For integer/None inputs, if the estimator is a classifier and y is either binary or multiclass, StratifiedKFold is used.
-grid = GridSearchCV(logisticReg, param_grid=param_grid_course, n_jobs=-1, cv=None, scoring=['accuracy'], return_train_score=True, refit='accuracy')
-grid.fit(X_gray_std_10_percent, y_10_percent)
-print("Grid-Search with accuracy")
-print("Best parameters:", grid.best_params_)
-print("Best cross-validation score (accuracy)): {:.3f}".format(grid.best_score_))
-print("CV scores {}".format(grid.cv_results_))
-
-cvresults_forfile = grid.cv_results_
-f = open("C:/Users/Yme/Documents/MEGA/Master DSE/Data Mining/Assignments/Assignment 1/saved_files/logreg_fine.pkl","wb")
-pickle.dump(cvresults_forfile,f)
-f.close()
+# SVHN = oml.datasets.get_dataset(41081)                                   #Load data
+# X, y, cats, attrs = SVHN.get_data(dataset_format='array',
+#     target=SVHN.default_target_attribute)
+#
+# X_gray_std = np.load("std_grayscale_data.npy")
+#
+# #Initialize default logistic regression
+# logisticReg = LogisticRegression()
+#
+# #Initialize default knn classifier
+# knn = KNeighborsClassifier()
+#
+# #Initialize default linear support vector machine
+# svc = LinearSVC()
+#
+# X_gray_std_90_percent, X_gray_std_10_percent, y_90_percent, y_10_percent = train_test_split(X_gray_std, y, test_size=0.1, stratify=y, random_state=47)
+#
+#
+# param_grid_course = {'C': [1e-5 ,1e-4, 1e-3, 1e-2 ,1e-1 ,1, 1e2, 1e3, 1e4, 1e5]}
+# param_grid2 = {'n_neighbors': [5,15,25,35,45]}
+# # using the default scoring of accuracy:
+#
+# #In GridSearchCV we use the default cv, as it implements Stratified K-folds cross-validation with three folds,
+# # From documentation: For integer/None inputs, if the estimator is a classifier and y is either binary or multiclass, StratifiedKFold is used.
+# grid = GridSearchCV(logisticReg, param_grid=param_grid_course, n_jobs=-1, cv=None, scoring=['accuracy'], return_train_score=True, refit='accuracy')
+# grid.fit(X_gray_std_10_percent, y_10_percent)
+# print("Grid-Search with accuracy")
+# print("Best parameters:", grid.best_params_)
+# print("Best cross-validation score (accuracy)): {:.3f}".format(grid.best_score_))
+# print("CV scores {}".format(grid.cv_results_))
+#
+# cvresults_forfile = grid.cv_results_
+# f = open("C:/Users/Yme/Documents/MEGA/Master DSE/Data Mining/Assignments/Assignment 1/saved_files/logreg_fine.pkl","wb")
+# pickle.dump(cvresults_forfile,f)
+# f.close()
---
+++ Assignment 2/Assignment2.py
@@ -0,0 +1,1 @@
+print("Hello world, This is assignment 2")

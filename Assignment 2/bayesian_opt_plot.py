73cc2d38cda9d3f9a99ecf16d22c6cd850adda74
Psychotechnopath <ymevdlinden@gmail.com>
2019-11-02 16:13:55 +0100

    Assignment 2, finished SVM classifier and regressor, scores can be found in .pkl files. Plots to be made.

--- Assignment 2/bayesian_opt_plot.py
+++ Assignment 2/bayesian_opt_plot.py
@@ -4,6 +4,8 @@ from pbreg import EI
 import numpy as np
 from matplotlib import pyplot as plt
 import openml as oml
+np.random.seed(47)
+
 
 robotnav = oml.datasets.get_dataset(1497)
 X_nav, y_nav, cats, attrs = robotnav.get_data(dataset_format='array',
@@ -16,19 +18,23 @@ sample_g = np.random.choice(gamma_points, 10, replace=False) #Sample 10 random p
 random_hyperparams = np.array(list(zip(sample_c, sample_g))) #Zip together sample_c and sample_g, cast it to the right data structure (np.array)
 c_points_gamma_fixed = np.array([(1, c) for c in c_points])  #Create a np.array where c varies and gamma is fixed
 
-with open('score_list.pkl', 'rb') as score_list:
-    unpickle_score_list = pickle.load(score_list)
+with open('score_list_svm.pkl', 'rb') as score_list:
+    unpickle_score_list_svm_classification = pickle.load(score_list)
 
-accuracy_means = np.array([1 - np.mean(i['test_accuracy']) for i in unpickle_score_list]) #Calculate the means out of the returned accuracy from the standard svm
+with open('score_list_regression.pkl', 'rb') as score_list:
+    unpickle_score_list_svm_regression = pickle.load(score_list)
+
+
+accuracy_means = np.array([1 - np.mean(i['test_accuracy']) for i in unpickle_score_list_svm_classification]) #Calculate the means out of the returned accuracy from the standard svm
+mse_means = np.array([np.mean(i['test_neg_mean_squared_error']) for i in unpickle_score_list_svm_regression])
+print(mse_means)
+
 
 rf = ProbabilisticRandomForestRegressor(n_estimators=100)
 rf.fit(random_hyperparams, accuracy_means)
 Y_pred, sigma = rf.predict(c_points_gamma_fixed, return_std=True)
-print(Y_pred)
-print(sigma)
-print(c_points)
-# Plot surrogate model
 
+# Plot surrogate model
 plt.ylabel('Surrogate model error')
 plt.xscale('log')
 plt.plot(c_points, Y_pred, label=u'Prediction')
@@ -36,9 +42,3 @@ plt.fill_between(c_points.ravel(),Y_pred-2*sigma,Y_pred+2*sigma,alpha=0.1,label=
 plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 plt.show()
 
-# Plot acquisition function
-# plt.subplot(2, 1, 2)
-# plt.xlabel('Hyperparameter value')
-# plt.ylabel('Expected Improvement')
-# plt.plot(c, EI(rf,Y_pred))
-# plt.show()
--- Assignment 2/standard_svm.py
+++ Assignment 2/standard_svm.py
@@ -1,9 +1,9 @@
 import openml as oml
 import numpy as np
-from scipy.stats import norm
-from sklearn.svm import SVC
+from sklearn.svm import SVC, SVR
 from sklearn.model_selection import train_test_split, cross_validate
 import pickle
+np.random.seed(47)
 
 # Download Wall Robot Navigation data from OpenML.
 robotnav = oml.datasets.get_dataset(1497)
@@ -19,17 +19,32 @@ c_points = np.logspace(-12,12, 25)
 gamma_points = np.logspace(-12, 12, 25)
 sample_c = np.random.choice(c_points, 10 , replace=False)
 sample_g = np.random.choice(gamma_points, 10, replace=False)
-random_hyperparams = zip(sample_c, sample_g)
+random_hyperparams = np.array(list(zip(sample_c, sample_g)))
 
-score_list = []
-
+score_list_svm_classifier = []
 for c_param, g_param in random_hyperparams:
     svc = SVC(C=c_param, kernel='rbf', gamma=g_param)
-    score = cross_validate(svc, X_nav, y_nav, cv=3, scoring=['mse'])
-    score_list.append(score)
+    score = cross_validate(svc, X_nav, y_nav, cv=3, scoring=['accuracy'])
+    score_list_svm_classifier.append(score)
 
-with open("score_list.pkl", "wb") as f:
-    pickle.dump(score_list, f)
+
+score_list_svr_regressor = []
+for c_param, g_param in random_hyperparams:
+    svr = SVR(C=c_param, kernel='rbf', gamma=g_param)
+    score = cross_validate(svr, Xr, yr, cv=3, scoring=['mean_squared_error'])
+    print(score)
+    score_list_svr_regressor.append(score)
+    print(score_list_svr_regressor)
+
+
+with open("score_list_svm.pkl", "wb") as f:
+    pickle.dump(score_list_svm_classifier, f)
+
+
+with open("score_list_regression.pkl", "wb") as f:
+    pickle.dump(score_list_svr_regressor, f)
+
+
 
 
 
